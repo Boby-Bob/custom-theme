1b0fcec3f4e95e34c94c4d3d28f78f412bd94155
Boby-Bob <kevin.schiller@hotmail.com>
2020-07-09 21:36:42 +0200

    Création répo

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <footer>
+        <ul>
+            <li><a href="<?php echo home_url(); ?>/page-d-exemple">Page d'exemple</a></li>
+            <li><a href="<?php echo home_url(); ?>/politique-de-confidentialite">Politique de confidentialité</a></li>
+            <li><a href="<?php echo home_url(); ?>/erff">erff</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,54 @@
+<?php
+
+get_header();
+
+
+// WP_Query arguments
+$args = [
+    'post_type' => [ 'page' ],
+];
+
+// The Query
+$query = new WP_Query( $args );
+
+if ( $query->have_posts() ):
+    while ($query->have_posts()):
+        $query->the_post();
+?>
+    <article>
+        <h3><a href="<?= get_permalink(); ?>"><?php the_title(); ?></a></h3>
+    </article>
+<?php
+    endwhile;
+endif;
+
+// Restore original Post Data
+wp_reset_postdata();
+
+?>
+<h2>liste des posts</h2>
+<?php
+
+// WP_Query arguments
+$args = [
+    'post_type' => [ 'post' ],
+];
+
+// The Query
+$query = new WP_Query( $args );
+
+if ( $query->have_posts() ):
+    while ($query->have_posts()):
+        $query->the_post();
+?>
+    <article>
+        <h3><a href="<?= get_permalink(); ?>"><?php the_title(); ?></a></h3>
+    </article>
+<?php
+    endwhile;
+endif;
+
+// Restore original Post Data
+wp_reset_postdata();
+
+get_footer();

9a571454b3c678ac00d7e9c44d5c714c1a566d39
Fanny <f.pringere@gmail.com>
2020-07-12 15:41:54 +0200

    pages contact et à propos, redirections pages, css

--- header.php
+++ header.php
@@ -30,137 +30,19 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="menu navbar-nav nav justify-content-center">
                 <li class="menu-item nav-item mx-5">
-                  <a class="nav-link active" href="#">Accueil</a>
+                  <a class="nav-link active" href="/">Accueil</a>
                 </li>
                 <li class="menu-item nav-item mx-5">
-                  <a class="nav-link" href="#">A Propos</a>
+                  <a class="nav-link" href="/a-propos">A Propos</a>
                 </li>
                 <li class="menu-item nav-item mx-5">
-                  <a class="nav-link" href="#">Galerie</a>
+                  <a class="nav-link" href="/#galerie">Galerie</a>
                 </li>
                 <li class="menu-item nav-item mx-5">
-                  <a class="nav-link" href="#">Contact</a>
+                  <a class="nav-link" href="/contact">Contact</a>
                 </li>
             </ul>
         </div>
     </nav>
-    <section class="benefits container">
-        
-        <h1 class="title__benefits">Nos Prestations</h1>
 
-        <div class="row">
-          <div class="col-md-5">
-            <div class="benefits__container">
-              <div class="benefits__picture">
-                <img src="wp-content/uploads/2020/06/bebe.jpg" class="picture" alt="..." />
-              </div>
-              <div class="benefits__body">
-                <h5 class="benefits__title">Lorem ipsum</h5>
-                <p class="benefits__text">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
-                unde eligendi ullam eum omnis hic libero laborum, pariatur
-                consequuntur non
-                </p>
-                <a href="#" class="btn btn-primary">Plus de détails</a>
-              </div>
-                
-            </div>
-          </div>
-        
-          <div class="col-md-2">
-            <div class="benefits__separator"></div>
-          </div>
-
-          <div class="col-md-5">
-            <div class="benefits__container">
-              <div class="benefits__picture">
-                <img src="wp-content/uploads/2020/06/bebe.jpg" class="picture" alt="..." />
-              </div>
-              <div class="benefits__body">
-                <h5 class="benefits__title">Lorem ipsum</h5>
-                <p class="benefits__text">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
-                unde eligendi ullam eum omnis hic libero laborum, pariatur
-                consequuntur non
-                </p>
-                <a href="#" class="btn btn-primary">Plus de détails</a>
-              </div>
-                
-            </div>
-          </div>
-
-        </div>
-        <div class="benefits__separator--horizontal"></div>
-            
-        <div class="row">
-            <div class="col-md-5">
-              <div class="benefits__container">
-                <div class="benefits__picture">
-                </div>
-                <div class="benefits__body">
-                  <h5 class="benefits__title">Lorem ipsum</h5>
-                  <p class="benefits__text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
-                  unde eligendi ullam eum omnis hic libero laborum, pariatur
-                  consequuntur non
-                  </p>
-                  <a href="#" class="btn btn-primary">Plus de détails</a>
-                </div>
-                  
-              </div>
-            </div>
-          
-            <div class="col-md-2">
-              <div class="benefits__separator"></div>
-            </div>
-  
-            <div class="col-md-5">
-              <div class="benefits__container">
-                <div class="benefits__picture">
-                  <img src="wp-content/uploads/2020/06/bebe.jpg" class="picture" alt="..." />
-                </div>
-                <div class="benefits__body">
-                  <h5 class="benefits__title">Lorem ipsum</h5>
-                  <p class="benefits__text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
-                  unde eligendi ullam eum omnis hic libero laborum, pariatur
-                  consequuntur non
-                  </p>
-                  <a href="#" class="btn btn-primary">Plus de détails</a>
-                </div>
-                  
-              </div>
-            </div>
-            <div class="benefits__separator--last"></div>
-
-        </section>
-
-        
-
-    <section class="gallery">
-    <h1 class="title__gallery">Galerie</h1>
-    <div class="container">
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="wp-content/uploads/2020/06/pexels-photo-325690.jpg" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="wp-content/uploads/2020/06/pexels-photo-3270224.jpg" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="wp-content/uploads/2020/06/pexels-photo-374781.jpg" class="d-block w-100" alt="...">
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-    </div>
-</section>
+    
